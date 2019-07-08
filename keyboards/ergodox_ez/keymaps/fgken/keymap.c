4b90899b4985c2f7e63080074f9b7467281ef6b1
fgken <fg.bell.tree.333@gmail.com>
2019-07-08 19:44:50 +0900

    change

--- keyboards/ergodox_ez/keymaps/fgken/keymap.c
+++ keyboards/ergodox_ez/keymaps/fgken/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      | Mute |CtlLft|CtlRgt|                                       |      |      |      |      |      |
+ *   |      |      | Mute |CtlLft|CtlRgt|                                       |WhDown| WhUp |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |VolDn |VolUp |
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
            KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_MPLY,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                    KC_WH_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS,
+                    KC_WH_D, KC_WH_U, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS,
   KC_TRNS, KC_BTN2, KC_BTN1
