11594e0e904802fe94af42462b7532d9f83029d4
fgken <fg.bell.tree.333@gmail.com>
2019-07-25 10:25:07 +0900

    update

--- keyboards/ergodox_ez/keymaps/fgken/keymap.c
+++ keyboards/ergodox_ez/keymaps/fgken/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |A / L2|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; /Cmd| Enter  |
- * |--------+------+------+------+------+------| Eisuu|           | Kana |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| End  |           | PgDwn|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space| Del  |------|       |------|Backspac|Enter |
- *                                 |      |      | End  |       | PgDn |e       |      |
+ *                                 |      |      |Eisuu |       | Kana |e       |      |
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox(
@@ -38,20 +38,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL,          KC_1,           KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,
   KC_TAB,          KC_Q,           KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),
   KC_LCTRL,        LT(MDIA, KC_A), KC_S,          KC_D,    KC_F,    KC_G,
-  KC_LSFT,         KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,    KC_LANG2,
+  KC_LSFT,         KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,    KC_END,
   LT(SYMB,KC_GRV), KC_QUOT,        LALT(KC_LSFT), KC_LEFT, KC_RGHT,
                                                            ALT_T(KC_APP), KC_LGUI,
                                                                           KC_HOME,
-                                                         KC_SPC, KC_DELT, KC_END,
+                                                         KC_SPC, KC_DELT, KC_LANG2,
   // right hand
   KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,     KC_0,           KC_MINS,
   TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,           KC_BSLS,
                 KC_H,    KC_J,    KC_K,    KC_L,     GUI_T(KC_SCLN), KC_ENT,
-  KC_LANG1,     KC_N,    KC_M,    KC_COMM, KC_DOT,   CTL_T(KC_SLSH), KC_RSFT,
+  KC_PGDN,     KC_N,    KC_M,    KC_COMM, KC_DOT,   CTL_T(KC_SLSH), KC_RSFT,
   KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
   KC_LALT, CTL_T(KC_ESC),
   KC_PGUP,
-  KC_PGDN, KC_BSPC, KC_ENT
+  KC_LANG1, KC_BSPC, KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
